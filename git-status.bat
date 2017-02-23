ae968bed467c2bdfe6831c24282bea70d15c3516
it-arjan <it-arjan@users.noreply.github.com>
2017-02-23 08:32:39 +0100

    new adds

---
+++ git-status.bat
@@ -0,0 +1,17 @@
+echo off
+cd IdentityServer3
+C:\Users\Arjan_2\AppData\Local\GitHub\PortableGit_f02737a78695063deace08e96d5042710d3e32db\cmd\git status
+
+cd ..
+cd MVCFrontend
+C:\Users\Arjan_2\AppData\Local\GitHub\PortableGit_f02737a78695063deace08e96d5042710d3e32db\cmd\git status
+
+cd ..
+cd NLogWrapper
+C:\Users\Arjan_2\AppData\Local\GitHub\PortableGit_f02737a78695063deace08e96d5042710d3e32db\cmd\git status
+
+cd ..
+cd WebEntrypoint
+C:\Users\Arjan_2\AppData\Local\GitHub\PortableGit_f02737a78695063deace08e96d5042710d3e32db\cmd\git status
+
+pause
